6890aec949ba7db36b2b61876994bc455d543cae
Karatsuyu <tabjulian07@gmail.com>
2025-10-06 17:07:44 -0500

    Estructura e Implementaciones Iniciales

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+  // Suavizar scroll al hacer clic en enlaces del menú
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      window.scrollTo({
+        top: target.offsetTop - 20,
+        behavior: 'smooth'
+      });
+    }
+  });
+});
